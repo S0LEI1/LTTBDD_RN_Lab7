37bc4eb6c08efe53e755e4bfc488cf3ac11f0c22
S0LEI1 <vanngocpentakill@gmail.com>
2023-11-03 15:47:56 +0700

    up navigator

---
+++ App.js
@@ -0,0 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LandingPage from './src/screen/LandingPage';
+import HomePage from './src/screen/HomePage';
+import DetailPage from './src/screen/DetailPage';
+const Stack = createNativeStackNavigator()
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name='Lading' component={LandingPage} />
+        <Stack.Screen name='Home' component={HomePage} />
+        <Stack.Screen name='Detail' component={DetailPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
